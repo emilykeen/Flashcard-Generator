788eedb188ca6f941f7277aa711ae2ad7bc27ae4
Emily Keen <emily.keen711@gmail.com>
2017-04-12 17:40:43 -0400

    almost done!

--- main.js
+++ main.js
@@ -1,30 +1,64 @@
 // Create a new GitHub repository, named Flashcard-Generator or something similar. Clone this to your local drive.
 // Create a BasicCard constructor. It should accept front and back arguments.
-function BasicCard(front,back) {
-  this.front = front;
-  this.back = back;
-}
+//add inquirer for prompts
+//add fs to append text
+var fs = require("fs");
+
+var cardType = process.argv[2];
+var first = process.argv[3];
+var second = process.argv[4];
+
+
+function BasicCard(front, back) {
+    this.front = front;
+    this.back = back;
+    var logCardTxt = "\nFront: " + front + " Back: " + back;
+    fs.appendFile("basicCard.txt", logCardTxt);
+    console.log("the front is: " + front);
+    console.log("The Back is: " + back);
+};
 // Create a ClozeCard constructor. It should accept text and cloze arguments.
-function ClozeCard(text,cloze) {
-  this.text = text;
-  this.cloze = cloze;
-  this.clozedDeleted =function(){}
-  this.partial = function(){
-  	text= text.replace(cloze,"...."); 
-  	console.log(text);
-  }
-  this.fullText = function(){}
+function ClozeCard(text, cloze) {
+    this.text = text;
+    this.cloze = cloze;
+    this.clozedDeleted = cloze;
+    this.partial = function() {
+        if (text.includes(cloze)) {
+            text = text.replace(this.cloze, "....");
+            return text;
+        }
+    }
+    this.fullText = text;
+    var logTxt = "\nText: " + text + " Cloze: " + cloze +
+        " partial: " + this.partial();
+    fs.appendFile("clozeCard.txt", logTxt);
+
+
+
+};
+
+//var newCardCreator = new CardCreator();
+if (cardType === "Basic Card") {
+
+    new BasicCard(first, second);
+
+} else {
+
+    new ClozeCard(first, second);
+
 }
 
- var firstPresident = new BasicCard(
-    "Who was the first president of the United States?", "George Washington");
- //console.log(firstPresident.front);
-// console.log(firstPresident.back);
+// var firstPresident = new BasicCard(
+//     "Who was the first president of the United States?", "George Washington");
+// //console.log(firstPresident.front);
+// //console.log(firstPresident.back);
 
-var firstPresidentCloze = new ClozeCard(
-    "George Washington was the first president of the United States.", "George Washington");
-console.log(firstPresidentCloze.cloze);
-console.log(firstPresidentCloze.partial);
+// var firstPresidentCloze = new ClozeCard(
+//     "George Washington was the first president of the United States.", "George Washington");
+// console.log(firstPresidentCloze.cloze);
+// console.log(firstPresidentCloze.partial());
+
+
 
 
 // ClozeCard should have a property or method that contains or returns only the cloze-deleted portion of the text.
