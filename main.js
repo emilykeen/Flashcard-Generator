64542a59616f32bc908a106aabea9077813281f8
Emily Keen <emily.keen711@gmail.com>
2017-04-13 13:41:00 -0400

    added comments

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
-// Create a new GitHub repository, named Flashcard-Generator or something similar. Clone this to your local drive.
-// Create a BasicCard constructor. It should accept front and back arguments.
-//add inquirer for prompts
 //add fs to append text
 var fs = require("fs");
 
+//setting node position to varibles 
 var cardType = process.argv[2];
 var first = process.argv[3];
 var second = process.argv[4];
 
-
+//basic card constructor with front and back arguments
 function BasicCard(front, back) {
     this.front = front;
     this.back = back;
+    //logging card to text file
     var logCardTxt = "\nFront: " + front + " Back: " + back;
     fs.appendFile("basicCard.txt", logCardTxt);
     console.log("the front is: " + front);
     console.log("The Back is: " + back);
 };
-// Create a ClozeCard constructor. It should accept text and cloze arguments.
+// ClozeCard constructor with text and cloze arguments.
 function ClozeCard(text, cloze) {
     this.text = text;
     this.cloze = cloze;
-    this.clozedDeleted = cloze;
     this.partial = function() {
         if (text.includes(cloze)) {
             text = text.replace(this.cloze, "....");
             return text;
+        } else {
+            console.log("What you want to cloze is not in the full statement!")
         }
     }
-    this.fullText = text;
+
+    //logging in a textfile
     var logTxt = "\nText: " + text + " Cloze: " + cloze +
         " partial: " + this.partial();
     fs.appendFile("clozeCard.txt", logTxt);
@@ -36,8 +37,7 @@ function ClozeCard(text, cloze) {
 
 
 };
-
-//var newCardCreator = new CardCreator();
+//determining what type of card it is. 
 if (cardType === "Basic Card") {
 
     new BasicCard(first, second);
@@ -57,15 +57,3 @@ if (cardType === "Basic Card") {
 //     "George Washington was the first president of the United States.", "George Washington");
 // console.log(firstPresidentCloze.cloze);
 // console.log(firstPresidentCloze.partial());
-
-
-
-
-// ClozeCard should have a property or method that contains or returns only the cloze-deleted portion of the text.
-// ClozeCard should have a property or method that contains or returns only the partial text.
-// ClozeCard should have a property or method that contains or returns only the full text.
-// ClozeCard should throw or log an error when the cloze deletion does not appear in the input text.
-// Use prototypes to attach these methods, wherever possible.
-// As your application will not have a front end, your only need to determine an efficient way to store cloze-deleted cards—you don't have to solve the problem of displaying them. 
-//You are free to decide how you'd like to implement this. One might represent the above flashcard thus:
-// The bulk of this assignment is implementing ClozeCard. If you build a robust ClozeCard implementation, feel free to try your hand at implementing a front-end, as well.
